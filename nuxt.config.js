4836f56f870a5eabb547cbb3df30b6ecd6b4fc6f
osd-02 <sakanaq0223@gmail.com>
2021-06-24 14:41:43 +0900

    vuetifyでのカラー変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,25 +6,21 @@ export default {
   head: {
     title: 'suisei-nuxt',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -33,30 +29,29 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/vuetify',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   vuetify: {
     theme: {
       light: true,
       themes: {
         light: {
-          main: "#93a9d1",
-          sub: "#9395d1",
-          primary: "#facf7d",
-          secondary: "#ccc8bf",
-          accent: "#bdd3e2",
-          warning: "#aa9e32",
-          success: "#0f4c81"
-        }
-      }
-    }
+          main: '#93a9d1',
+          sub: '#9395d1',
+          primary: '#facf7d',
+          secondary: '#ccc8bf',
+          accent: '#bdd3e2',
+          warning: '#aa9e32',
+          success: '#0f4c81',
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,8 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">suisei-nuxt</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <v-sheet color="main">
+      <div></div>
+    </v-sheet>
   </div>
 </template>
 
