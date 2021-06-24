f5c73fdc83cad4bfb7a44119bd08487f8730f51a
osd-02 <sakanaq0223@gmail.com>
2021-06-24 16:12:26 +0900

    headerの実装

---
+++ components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="home-logo-wrapper">
+    <v-app-bar-nav-icon
+      id="drawer-icon"
+      @click.stop="drawer = !drawer"
+    ></v-app-bar-nav-icon>
+    <img id="home-logo" src="../assets/img/home-logo.gif" />
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#home-logo-wrapper {
+  position: fixed;
+  display: flex;
+  height: 56px;
+  #drawer-icon {
+    margin: 10px 0;
+    .v-icon {
+    }
+  }
+  #home-logo {
+    padding: 13px 0;
+    margin: 0 auto;
+  }
+}
+</style>
