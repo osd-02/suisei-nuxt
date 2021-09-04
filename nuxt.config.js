5ecfe5f02ed4bcd5d97db8b936523025e2c1dc41
osd-02 <sakanaq0223@gmail.com>
2021-09-04 21:49:18 +0900

    heroku導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  ssr: true,
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
